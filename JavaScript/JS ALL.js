b08d8bed985a59b8a7e1dfd13cf363e4d04ace62
datdev27 <nguyendatz0207@gmail.com>
2023-10-06 09:26:58 +0700

    JS Firt

---
+++ JavaScript/JS ALL.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <tile>1st JavaScript</tile>
+    <script>
+        alert(welcome to JS by Nguyen Tien Dat);
+
+    </script>
+ </head> 
+ <body>
+</body>  
+</html>
