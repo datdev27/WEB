31341fd11fb0dfaf463f727ab91adcaebfeda075
datdev27 <nguyendatz0207@gmail.com>
2023-10-27 08:57:41 +0700

    thay đổi thuộc tính

--- JavaScript/JS ALL.js
+++ JavaScript/JS ALL.js
@@ -1,15 +1,34 @@
-<!DOCTYPE html>
-<html>
- <head>
-    <tile>1st JavaScript</tile>
-    <script>
-    function hello() {
-        alert(welcome to JS by Nguyen Tien Dat);
-        }
-    </script>
- </head> 
- <body>
- <h1>1st JavaScript</h1>
-    <button onclick="hello()">Click Here !</button>
-</body>  
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <head>
+        <script>
+        document.addEventListener('DOMContentLoaded',function(){
+            document.querySelector('#red').onclick=function(){
+                document.querySelector('#hello').style.color='red';
+            };
+            //red
+            document.querySelector('#blue').onclick=function(){
+                document.querySelector('#hello').style.color='blue';
+            };
+            //blue
+            document.querySelector('#green').onclick=function(){
+                document.querySelector('#hello').style.color='green';
+            };
+            //green
+        });
+        </script>
+      </head>
+</head>
+<body>
+    <h1 id="hello">Hello!</h1>
+    <button id="red">Red</button>
+    <button id="blue">Blue</button>
+    <button id="green">Green</button>
+
+
+
+</body>
 </html>
