762ee8682268e249f7341333d16afd4c244c6445
datdev27 <nguyendatz0207@gmail.com>
2023-11-10 11:24:44 +0700

    PHP

---
+++ PHP/5-FORMGET.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en", dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    Welcome <?php echo $_GET["name"]; ?><br>
+    Your E-mail address is: <?php echo $_GET["email"]; ?>
+</body>
+</html>
